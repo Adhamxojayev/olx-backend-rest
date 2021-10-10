0fb1e0cef6f61b27a63a0bc35114f022a2b55c1c
Ahror Adhamxojayev <axroradhamxojayev@gmail.com>
2021-10-10 14:04:45 +0500

    backend

--- src/controller/ads.js
+++ src/controller/ads.js
@@ -12,11 +12,7 @@ const ADD = async (req,res) => {
         req.body.user_id = user.userId
         let added = await adsAdd(req.body)
         if(added.length){
-            res.json({
-                status: 201,
-                message: "reklama qo'shildi",
-                data: added
-            })
+            res.redirect('http://localhost:4000/')
         }
         else throw new Error("reklama qo'shishda hatolik")
     } catch (error) {
